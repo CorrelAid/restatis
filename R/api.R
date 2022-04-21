03454af21ec71d2ceccf248e0966e41aef6f469d
long.nguyen <long.nguyen@uni-bielefeld.de>
2022-04-21 18:11:48 +0200

    Specify user agent for requests

--- R/api.R
+++ R/api.R
@@ -9,6 +9,7 @@
 #'   httr2::resp_body_json()
 gen_api <- function(endpoint, ...) {
   httr2::request("https://www-genesis.destatis.de/genesisWS/rest/2020") |>
+    httr2::req_user_agent("https://github.com/CorrelAid/restatis") |>
     httr2::req_url_path_append(endpoint) |>
     httr2::req_url_query(...) |>
     httr2::req_retry(max_tries = 3) |>
