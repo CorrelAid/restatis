36ae3aa7cf712b81406887f3e1d2a15f36fa1754
Dorian <d.le-jeune@gmx.de>
2022-08-22 17:36:06 +0200

    Added functions for input checks

--- R/utils.R
+++ R/utils.R
@@ -13,3 +13,22 @@ str_collapse <- function(x) {
   stopifnot(is.character(x))
   paste0(x, collapse = ",")
 }
+
+lang_check <- function(x) {
+  if(is.null(x)){
+    language = Sys.getenv("LANG")
+  }
+  if(!(x %in% c("de","eng"))){
+    stop("Available languages are German (de) or English (eng).")
+  }
+}
+
+type_check <- function(x) {
+  if(is.null(x)){
+    type = "alle"
+    message("No type was specified so all types will be included.")
+  }
+  if(!(x %in% c("klassifizierend", "insgesamt", "räumlich", "sachlich", "wert", "zeitlich", "zeitidentifizierend", "alle"))){
+    stop("One of the following types must be specified: klassifizierend, insgesamt, räumlich, sachlich, wert, zeitlich, zeitidentifizierend, alle.")
+  }
+}
