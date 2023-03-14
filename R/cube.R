e0a1a84ecc6f8820f82440b88456ac94f2f0dff3
Johannes Ritter <johannes.ritter@bynd.consulting>
2023-03-14 08:36:35 +0100

    merged with table added parameters

--- R/cube.R
+++ R/cube.R
@@ -1,14 +1,41 @@
 #' Get a data cube
 #'
+#' @description Download a cube with data from Genesis
+#'
 #' @param name Name of the data cube
-#' @param values Logical: Should values be included?
-#' @param metadata Logical: Should metadata be included?
-#' @param contents Names of required statistical specifications
-#' @param startyear Start of time period
-#' @param endyear End of time period
-#' @param timeslices Number of timeslices (cumulative to startyear or endyear)
-#' @param language Either "de" or "en"
-#' @param ... Additional parameters can be specified (Cf. https://www-genesis.destatis.de/genesis/misc/GENESIS-Webservices_Einfuehrung.pdf)
+#' @param ... Optional parameters passed on to the Genesis API call:
+#'   \describe{
+#'     \item{\code{area}}{a string. The area in which the table is stored. Possible values:
+#'     \itemize{
+#'       \item \code{"public"}: cube in the public catalogue
+#'       \item \code{"user"}: cube in the user's account
+#'     }}
+#'     \item{\code{values}}{a logical. Should values be included?}
+#'     \item{\code{metadata}}{a logical. Should metadata be included?}
+#'     \item{\code{additionals}}{a logical. Should additional metadata be included?}
+#'     \item{\code{contents}}{a string. Names of required statistical specifications}
+#'     \item{\code{startyear,endyear}}{a number. Only retrieve data between these years.}
+#'     \item{\code{timeslices}}{a number. Number of timeslices (cumulative to startyear or endyear)}
+#'     \item{\code{regionalvariable}}{character. Code of the regional variable
+#'     whose value is specified in \code{regionalkey} to filter the results.}
+#'     \item{\code{regionalkey}}{character. One or more regional keys. Multiple
+#'       values can be supplied as a character vector or as a single string,
+#'       with the regional keys separated by commas. Use of wildcard (`*`) allowed.}
+#'     \item{\code{classifyingvariable1,classifyingvariable2
+#'       ,classifyingvariable3}}{character. Code of the subject classification
+#'       (SK-Merkmal) to which the selection by means of the corresponding
+#'       `classifyingkey` parameter is to be applied.}
+#'     \item{\code{classifyingkey1,classifyingkey2,classifyingkey3}}{character.
+#'       One or more values of a subject classification (e.g. "WZ93012"). Applied
+#'       to the corresponding `classifyingvariable` parameter. Multiple
+#'       keys can be supplied as a character vector or as a single string,
+#'       with the keys separated by commas. Use of wildcard (`*`) allowed.}
+#'     \item{\code{stand}}{a string \code{"DD.MM.YYYY"}. Only retrieve data
+#'       updated after this #' date.}
+#'     \item{\code{language}}{Search terms, returned messages and data
+#'       descriptions in German (`"de"`) or English (`"en"`)?}
+#'   }
+#'
 #'
 #' @export
 #'
@@ -17,15 +44,61 @@
 #' gen_cube("47414BJ002")
 #' }
 gen_cube <- function(name, ...) {
-  cube <- gen_api("data/cubefile", name = name, values = TRUE,
-                  metadata = TRUE, contents = NULL,  startyear = NULL,
-                  endyear = NULL, timeslices = NULL, ...) %>%
+  gen_cube_(name, ...)
+}
 
-    check_table_input( name = name,
-                       compress = compress,
-                       startyear = startyear,
-                       endyear = endyear,
-                       timeslices = timeslices)
+gen_cube_ <- function(name,
+                      area = c("public", "user"),
+                      values = TRUE,
+                      metadata = TRUE,
+                      additionals = FALSE,
+                      startyear = 1900,
+                      endyear = 2100,
+                      timeslices = NULL,
+                      contents = NULL,
+                      regionalvariable = NULL,
+                      regionalkey = NULL,
+                      classifyingvariable1 = NULL,
+                      classifyingkey1 = NULL,
+                      classifyingvariable2 = NULL,
+                      classifyingkey2 = NULL,
+                      classifyingvariable3 = NULL,
+                      classifyingkey3 = NULL,
+                      stand = NULL,
+                      language = Sys.getenv("GENESIS_LANG")) {
+  area <- match.arg(area)
+  if (!isTRUE(language == "en")) {
+    area <- switch(area, public = "öffentlich", user = "benutzer")
+  }
+  param_check_year(startyear)
+  param_check_year(endyear)
+  contents <- param_collapse_vec(contents)
+  regionalkey <- param_collapse_vec(regionalkey)
+  classifyingkey1 <- param_collapse_vec(classifyingkey1)
+  classifyingkey2 <- param_collapse_vec(classifyingkey2)
+  classifyingkey3 <- param_collapse_vec(classifyingkey3)
+
+  cube <- gen_api("data/cubefile", name = name,
+                  name = name,
+                  area = area,
+                  values = values,
+                  metadata = metadata,
+                  additionals = additionals,
+                  startyear = startyear,
+                  endyear = endyear,
+                  timeslices = timeslices,
+                  contents = contents,
+                  regionalvariable = regionalvariable,
+                  regionalkey = regionalkey,
+                  classifyingvariable1 = classifyingvariable1,
+                  classifyingkey1 = classifyingkey1,
+                  classifyingvariable2 = classifyingvariable2,
+                  classifyingkey2 = classifyingkey2,
+                  classifyingvariable3 = classifyingvariable3,
+                  classifyingkey3 = classifyingkey3,
+                  stand = stand,
+                  language = language,
+                  job = FALSE) %>%
 
     read_cube() %>%
     rename_cube_data_columns()
