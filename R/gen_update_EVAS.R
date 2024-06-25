6552fe661c43cc09d5c1d7958f61eeaf1748d6b3
KovaZo <kovacevic.zoran@gmx.de>
2024-06-25 11:09:30 +0200

    update regio link and package purrr/rvest

--- R/gen_update_EVAS.R
+++ R/gen_update_EVAS.R
@@ -7,19 +7,31 @@
 #'
 gen_update_EVAS <- function(){
 
+  # Check rvest and purrr packages
+  if (!requireNamespace("rvest", quietly = TRUE)) {
+
+    stop("If you want to use this specific function, the package {rvest} needs to be installed.",
+         call. = FALSE)
+
+  }
+
+  if (!requireNamespace("purrr", quietly = TRUE)) {
+
+    stop("If you want to use this specific function, the package {purrr} needs to be installed.",
+         call. = FALSE)
+
+  }
+
   # Path selection
   data_path <- system.file("data", "EVAS_numbers.RData", package = "restatis")
-
-  # Load the data object
-  data("EVAS_numbers", package = "restatis")
 
   # Define the src URL of the EVAS numbers
   url <- "https://erhebungsdatenbank.estatistik.de/eid/TabelleEvas.jsp"
 
   # Read the HTML content of the URL
-  html <- read_html(url)
-  html <- html_nodes(html, "table")
-  html <- map_dfr(html, html_table, convert = FALSE)
+  html <- rvest::read_html(url)
+  html <- rvest::html_nodes(html, "table")
+  html <- purrr::map_dfr(html, html_table, convert = FALSE)
   html <- html[, c("EVAS", "Beschreibung")]
   html$Titel <- paste(html$EVAS, html$Beschreibung, sep = " - ")
   attr(html, "Update_Date") <- format(Sys.Date(), "%Y%m%d")
