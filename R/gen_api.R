92a9c1b70592c1b21e19e4e5aaa0e10f784e7801
buhly <yannik.buhl@posteo.de>
2024-08-30 11:03:54 +0200

    begin move to POST

--- R/gen_api.R
+++ R/gen_api.R
@@ -14,12 +14,42 @@
 #'   httr2::resp_body_json()
 #' }
 #'
+# gen_api <- function(endpoint, ...) {
+#
+#   httr2::request("https://www-genesis.destatis.de/genesisWS/rest/2020") %>%
+#     httr2::req_user_agent("https://github.com/CorrelAid/restatis") %>%
+#     httr2::req_url_path_append(endpoint) %>%
+#     httr2::req_url_query(!!!gen_auth_get(database = "genesis"), ...) %>%
+#     httr2::req_retry(max_tries = 3) %>%
+#     httr2::req_perform()
+#
+# }
+
 gen_api <- function(endpoint, ...) {
 
-  httr2::request("https://www-genesis.destatis.de/genesisWS/rest/2020") %>%
-    httr2::req_user_agent("https://github.com/CorrelAid/restatis") %>%
+  url <- "https://www-genesis.destatis.de/genesisWS/rest/2020"
+  user_agent <- "https://github.com/CorrelAid/restatis"
+
+  body_parameters <- list(...)
+
+  if (length(body_parameters) > 0) {
+
+    req <- httr2::request(url) %>%
+      httr2::req_body_form(!!!body_parameters)
+
+  } else {
+
+    req <- httr2::request(url) %>%
+      httr2::req_body_form(!!!list("foo" = "bar"))
+
+  }
+
+  req %>%
+    httr2::req_user_agent(user_agent) %>%
     httr2::req_url_path_append(endpoint) %>%
-    httr2::req_url_query(!!!gen_auth_get(database = "genesis"), ...) %>%
+    httr2::req_headers("Content-Type" = "application/x-www-form-urlencoded",
+                       "username" = gen_auth_get(database = "genesis")$username,
+                       "password" = gen_auth_get(database = "genesis")$password) %>%
     httr2::req_retry(max_tries = 3) %>%
     httr2::req_perform()
 
